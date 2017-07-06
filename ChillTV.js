886041284136319d032f439237e752024edb3b42
Lisztinator <bdpaul91@gmail.com>
2017-07-05 21:27:29 -0400

    Update ChillTV.js

--- ChillTV.js
+++ ChillTV.js
@@ -3920,7 +3920,7 @@ function injectKickass() {
 
 function execEmotes(e) {
     return USEROPTS.no_emotes ? e : CyTube.featureFlag && CyTube.featureFlag.efficientEmotes ? execEmotesEfficient(e) : (CHANNEL.emotes.forEach(function(t) {
-        e = e.replace(t.regex, '$1<img class="channel-emote" src="' + t.image + '" title="' + t.name + '"> ')
+        e = e.replace(t.regex, '$1<a href="' + t.image + '" target="_blank" title="' + t.name + '"><img class="channel-emote" src="' + t.image + '" target="_blank"> ')
     }),
     e)
 }
